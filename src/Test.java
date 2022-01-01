6d3068f3c6bbb9894f1302da624d5d25cd63edd0
AllSetSimon <allsetsimon@gmail.com>
2022-01-02 01:35:59 +0900

    change

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        int result = Max_custom.maxi(1,3,2);
-        int medi = Median_custom.medi(4,2,7);
+        int result = Max_custom.maxi(1,3,2); // find maximum number
+        int medi = Median_custom.medi(4,2,7); // find median number
         System.out.println(result);
         System.out.println(medi);
     }
